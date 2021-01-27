f18fda1eb710fd6462b4ec79c5ba9a089916feba
dharmeshkumar1409 <73641326+dharmeshkumar1409@users.noreply.github.com>
2021-01-27 16:09:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup() {
 
     mb = createSprite(random(20, 570), 50, 30, 30);
     mb.shapeColor = "white";
-    mb.setVelocity(5, 4);
+    mb.setVelocity(4, 5);
 
 }
 
@@ -41,10 +41,10 @@ function draw() {
     background(200);
 
     //create edgeSprite
-    mb.bounceOff(box1);
-    mb.bounceOff(box2);
-    mb.bounceOff(box3);
-    mb.bounceOff(box4);
+    // mb.bounceOff(box1);
+    // mb.bounceOff(box2);
+    // mb.bounceOff(box3);
+    // mb.bounceOff(box4);
     mb.bounceOff(rightEdge);
     mb.bounceOff(leftEdge);
     mb.bounceOff(topEdge);
@@ -53,15 +53,22 @@ function draw() {
 
     //add condition to check if box touching surface and make it box
 
-    if (box1.isTouching(mb)) {
-        music.play();
+    if (box1.isTouching(mb) && mb.bounceOff(box1)) {
+        // music.play();
         mb.shapeColor = ("blue");
-    } else if (box2.isTouching(mb)) {
+        // mb.setVelocity(0, 0);
+    } else if (box2.isTouching(mb) && mb.bounceOff(box2)) {
+        // music.play();
         mb.shapeColor = ("green");
-    } else if (box3.isTouching(mb)) {
+        // mb.setVelocity(0, 0);
+    } else if (box3.isTouching(mb) && mb.bounceOff(box3)) {
+        // music.play();
         mb.shapeColor = ("red");
-    } else if (box4.isTouching(mb)) {
+        mb.setVelocity(0, 0);
+    } else if (box4.isTouching(mb) && mb.bounceOff(box4)) {
+        music.play();
         mb.shapeColor = ("yellow");
+        // mb.setVelocity(0, 0);
     }
 
     drawSprites();
